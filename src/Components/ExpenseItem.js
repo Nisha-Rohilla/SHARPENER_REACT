1093412dbf11fab69feff5ab1ca60ba0704e1d2d
Nisha-Rohilla <rohilla.nisha15@gmail.com>
2023-09-10 14:46:12 +0530

    first commit

---
+++ src/Components/ExpenseItem.js
@@ -0,0 +1,13 @@
+export function ExpenseItem(){
+    return(
+        <div>
+            <div>
+                <h2>ExpenseItem</h2>
+                <p>Food Rs 10</p>
+                
+                <p>Movies Rs 100</p>
+                <p>Petrol Rs 200</p>
+            </div>
+        </div>
+    )
+}
